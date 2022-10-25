bdf6c6c0d87066bb6f21ce7d2de471148b02ef0b
dave92 <dvd_homer@hotmail.com>
2022-10-25 11:14:18 +0200

    5.add navigation por la mitad

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Router, RouterLink } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Tour of Heroes';
+
+  constructor(private router:Router) { }
+
+  goTo(route:string){
+    this.router.navigate([route])
+  }
 }
