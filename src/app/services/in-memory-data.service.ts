f1d85f2a16678f2c5b9572fa2fe69e093aad1258
dave92 <dvd_homer@hotmail.com>
2022-11-02 10:19:38 +0100

    trasteos con bootstrap

--- src/app/services/in-memory-data.service.ts
+++ src/app/services/in-memory-data.service.ts
@@ -33,12 +33,12 @@ export class InMemoryDataService implements InMemoryDbService {
       { userId: 1, id: 101, title: 'explicabo enim cumque porro aperiam occaecati minima', completed: false },
       { userId: 2, id: 108, title: 'a eos eaque nihil et exercitationem incidunt delectus', completed: true },
       { userId: 1, id: 10, title: 'illo est ratione doloremque quia maiores aut', completed: true },
-      { userId: 1, id: 16, title: 'accusamus eos facilis sint et aut voluptatem', completed: true },
-      { userId: 2, id: 30, title: 'nemo perspiciatis repellat ut dolor libero commodi blanditiis omnis', completed: true },
-      { userId: 2, id: 30, title: 'nemo perspiciatis repellat ut dolor libero commodi blanditiis omnis', completed: true },
-      { userId: 2, id: 30, title: 'nemo perspiciatis repellat ut dolor libero commodi blanditiis omnis', completed: true },
-      { userId: 2, id: 30, title: 'nemo perspiciatis repellat ut dolor libero commodi blanditiis omnis', completed: true },
-      { userId: 2, id: 30, title: 'nemo perspiciatis repellat ut dolor libero commodi blanditiis omnis', completed: true },
+      { userId: 1, id: 16, title: 'accusamus eos facilis sint et aut voluptatem', completed: false },
+      { userId: 2, id: 31, title: 'nemo perspiciatis repellat ut dolor libero commodi blanditiis omnis', completed: true },
+      { userId: 1, id: 32, title: 'nemo perspiciatis ut dolor libero commodi blanditiis omnis', completed: true },
+      { userId: 2, id: 33, title: 'nemo perspiciatis repellat ut dolor libero comnditiis omnis', completed: false },
+      { userId: 1, id: 34, title: 'nemo perspiciatis repellat ut dolor libero commodi blanditiis omnis', completed: true },
+      { userId: 2, id: 35, title: 'nemo perspiciatis repellatibero commodi blanditiis omnis', completed: true },
     ];
 
     return {heroes, users, tareas}
